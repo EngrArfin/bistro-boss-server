e0cb19f0fe035bbc00126c50fc3804d7f23a833d
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-09-05 11:12:55 +0600

    server data post mongo for cart

--- index.js
+++ index.js
@@ -29,15 +29,25 @@ async function run() {
 
     const menuCollection = client.db("BistroDb").collection("menu");
     const reviewsCollection = client.db("BistroDb").collection("reviews");
+    const cartCollection = client.db("BistroDb").collection("carts");
 
     app.get('/menu', async(req, res) =>{
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
+
     app.get('/reviews', async(req, res) =>{
       const result = await reviewsCollection.find().toArray();
       res.send(result);
     })
+
+    app.post('/carts', async(req, res) =>{
+      const item = req.body;
+      console.log(item);git stat
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
